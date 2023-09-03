17fa764c929e88539b5aade0d28390ebdc0fb068
mohi14 <mohiuddinkhan1409@gmail.com>
2023-09-03 19:39:54 +0600

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+        fontFamily: {
+            'Unbounded': ['Unbounded', 'sans-serif'],
+            'Poppins': ['Poppins', 'sans-serif'],
+        }
+    }
+}
